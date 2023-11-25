0ab7527ea4e4a6b005191990fec3f8198c12f80f
Natalya Babenko <natapervomaisk88@gmail.com>
2023-11-25 11:06:22 +0200

    Added logger

---
+++ Lesson_25_11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+
+namespace Lesson_25_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = LogManager.GetCurrentClassLogger();
+            logger.Info("Первая запись в лог файле");
+            logger.Warn("Была небольшая ошибка");
+        }
+    }
+}
